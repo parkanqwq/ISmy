01cc01b1fb9fd563b216c90d6b565b5a5c76d5eb
Maksim Kalabukhov <bfmaximmm1992@gmail.com>
2020-10-07 18:05:26 +0300

    добавление кнопки и функционала удаления чата

--- app/src/main/java/com/example/ismy/AllChat.java
+++ app/src/main/java/com/example/ismy/AllChat.java
@@ -3,6 +3,9 @@ package com.example.ismy;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ismy.Adapter.AdapterAllChat;
 import com.example.ismy.BDHelper.DBHelper;
+import com.example.ismy.BDHelper.DBHelperChat;
 import com.example.ismy.Model.Chat;
 
 import java.util.ArrayList;
@@ -18,8 +22,10 @@ import java.util.List;
 public class AllChat extends AppCompatActivity {
 
     public DBHelper dbHelper;
+    public DBHelperChat dbHelperChat;
     AdapterAllChat adapterAllChat;
     List<Chat> mChat;
+    Button chatClear;
     RecyclerView recycler_view_all;
 
     @Override
@@ -28,12 +34,15 @@ public class AllChat extends AppCompatActivity {
         setContentView(R.layout.activity_all_chat);
 
         dbHelper = new DBHelper(this);
-
+        dbHelperChat = new DBHelperChat(this);
+        chatClear = findViewById(R.id.chatClear);
         recycler_view_all = findViewById(R.id.recycler_view_all);
         recycler_view_all.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         linearLayoutManager.setStackFromEnd(true);
         recycler_view_all.setLayoutManager(linearLayoutManager);
+
+
 
         mChat = new ArrayList<>();
         mChat.clear();
@@ -64,5 +73,19 @@ public class AllChat extends AppCompatActivity {
 
         adapterAllChat = new AdapterAllChat(AllChat.this, mChat);
         recycler_view_all.setAdapter(adapterAllChat);
+
+        chatClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteChat();
+            }
+        });
+    }
+
+    private void deleteChat(){
+
+        SQLiteDatabase database2 = dbHelperChat.getWritableDatabase();
+        database2.delete(DBHelperChat.TABLE_CONTACTS, null, null);
+        Toast.makeText(this, "Чат удален!", Toast.LENGTH_SHORT).show();
     }
 }
